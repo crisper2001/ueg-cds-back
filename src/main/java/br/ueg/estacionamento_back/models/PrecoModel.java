5b01fc65bad21d24e050ae73180ce7fafc79233c
MathiasBelmont <matheusbrenner@gmail.com.br>
2024-10-18 22:11:37 -0300

    mudanças de preços

---
+++ src/main/java/br/ueg/estacionamento_back/models/PrecoModel.java
@@ -0,0 +1,44 @@
+package br.ueg.estacionamento_back.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Data
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "PRECOS")
+public class PrecoModel implements IGenericModel<Long> {
+
+    @Id
+    @SequenceGenerator(
+            name = "precos_sequence",
+            sequenceName = "precos_sequence_bd",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "precos_sequence"
+    )
+    private Long id;
+
+    @NotNull
+    @Column(name = "tempo_inicial", nullable = false)
+    private int tempoInicial;
+
+    @NotNull
+    @Column(name = "precos_inicial", nullable = false)
+    private float precosInicial;
+
+    @NotNull
+    @Column(name = "precos_adicional", nullable = false)
+    private float precosAdicional;
+
+}
