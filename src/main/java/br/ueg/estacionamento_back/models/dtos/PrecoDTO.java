6de0f94faf64aeea3d6436fa2be78c149f18e336
Isaque <crisperohman@gmail.com>
2024-12-04 11:38:08 +0000

    MOD: Valores de exemplo em 'PrecoDTO' alterados

--- src/main/java/br/ueg/estacionamento_back/models/dtos/PrecoDTO.java
+++ src/main/java/br/ueg/estacionamento_back/models/dtos/PrecoDTO.java
@@ -7,19 +7,19 @@ import lombok.Data;
 @Data
 public class PrecoDTO {
 
-    @Schema(description = "O tempo inicial em minutos", example = "30", required = true)
+    @Schema(description = "O tempo inicial em minutos", example = "60", required = true)
     @NotBlank
     private Integer tempoInicial;
 
-    @Schema(description = "O tempo adicional em minutos", example = "15", required = true)
+    @Schema(description = "O tempo adicional em minutos", example = "30", required = true)
     @NotBlank
     private Integer tempoAdicional;
 
-    @Schema(description = "O valor inicial em reais", example = "10.00", required = true)
+    @Schema(description = "O valor inicial em reais", example = "0.00", required = true)
     @NotBlank
     private Double valorInicial;
 
-    @Schema(description = "O valor adicional em reais", example = "5.00", required = true)
+    @Schema(description = "O valor adicional em reais", example = "10.00", required = true)
     @NotBlank
     private Double valorAdicional;
 }
